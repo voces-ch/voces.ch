ef3cb9983e9e06847758228b7827cc6b325bba7e
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-04-07 15:46:43 +0200

    Add widget demo

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/login-redirect', '/login')->name('login');
 
+Route::get('/widget-demo', function () {
+    if (app()->environment('production')) {
+        abort(404);
+    }
+    return view('widget');
+})->name('widget');
+
