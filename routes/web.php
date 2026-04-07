ba6d087f4be0d1506b8e6e81b3f2495d052ae0f8
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-04-07 15:48:12 +0200

    Change route to account for route hierarchy

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/login-redirect', '/login')->name('login');
-
-Route::get('/widget-demo', function () {
+Route::get('/demo/widget/view', function () {
     if (app()->environment('production')) {
         abort(404);
     }
     return view('widget');
 })->name('widget');
+
+Route::redirect('/login-redirect', '/login')->name('login');
+
 
