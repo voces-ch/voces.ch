68d50665a21168021fb54d179c563359d1053195
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-04-12 00:50:55 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,8 @@ php artisan migrate --force
 # Cache configuration, routes, and views for a massive performance boost
 php artisan optimize
 
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 echo "Deployment scripts finished. Starting Apache..."
 # Hand off control to the main Docker command (Apache)
 exec "$@"
